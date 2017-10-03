752389ca4c83fff89edce2a5f89d2dbb67842ebb
ducks <mschaeffer53@gmail.com>
2017-10-03 12:01:25 -0700

    Initial revision

---
+++ python/lab1_turtle.py
@@ -0,0 +1,40 @@
+from turtle import *
+
+circle(50)
+left(270)
+forward(50)
+left(90)
+forward(50)
+backward(100)
+forward(50)
+left(90)
+backward(50)
+setheading(225)
+forward(50)
+backward(50)
+setheading(315)
+forward(50)
+"""
+
+fillcolor('green')
+
+begin_fill()
+
+x = 100
+y = 144
+
+
+
+forward(x)
+left(y)
+forward(x)
+left(y)
+forward(x)
+left(y)
+forward(x)
+left(y)
+
+end_fill()
+
+"""
+done()

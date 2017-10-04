d01345b172c10eaeb4baf6948d572ebc9c9d195a
ducks <mschaeffer53@gmail.com>
2017-10-04 11:49:50 -0700

    Initial revision

---
+++ python/lab9_makechange.py
@@ -0,0 +1,34 @@
+'''
+Lab 9 - Make Change
+Marcel Schaeffer
+10/4/17
+'''
+
+#user input
+coins = int(input("Type an amount of pennies: "))
+
+#coin values
+quarter = 25
+dime = 10
+nickle = 5
+penny = 1
+
+#math
+quarters = coins//quarter
+print(quarters)
+print("you have " + str(quarters) + " quarters")
+
+x = coins - quarter*quarters
+
+dimes = x//dime
+print('you have ' + str(dimes) + ' dimes')
+
+y = x - dime*dimes
+
+nickles = y//nickle
+print('you have ' + str(nickles) + ' nickles')
+
+z = y - nickle*nickles
+
+pennies = z//penny
+print('you have ' + str(pennies) + ' pennies')

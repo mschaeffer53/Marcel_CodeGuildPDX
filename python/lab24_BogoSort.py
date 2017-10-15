02c97f8e0b4a365773952ede821c0ec451381e39
ducks <mschaeffer53@gmail.com>
2017-10-15 15:49:31 -0700

    Initial revision

---
+++ python/lab24_BogoSort.py
@@ -0,0 +1,46 @@
+'''
+Lab 24 - Bogo Sort
+Marcel Schaeffer
+10/13/17
+'''
+
+#function that makes random list of numbers between 0-99 with a length of n
+def random_list(n):
+    import random
+    rand_list = []
+    i = 0
+    while i < n:
+        rand_list.append(random.randint(0,99))
+        i += 1
+    return rand_list
+
+
+#function to shuffle nums in list
+def shuffle(nums):
+    from random import shuffle
+    shuffle(nums)
+    return nums
+
+
+#is sorted
+def is_sorted(nums):
+    for i in range(len(nums) - 1):
+        if nums[i] > nums[i + 1]:
+            return False #if any number is larger than the next then False
+    return True #if nothing in list is False then True
+
+#bogo sort
+def bogosort(nums):
+    sort = False
+    while sort is False:
+        shuffle(nums) #shuffle list of nums
+        sort = is_sorted(nums) #nums is true if sorted else false
+        if sort is True:
+            print(nums)
+            return nums
+
+#run functions
+n = int(input('Type a number that represents the length of the random list: '))
+nums = random_list(n) #make list of (n) length
+print(nums)
+bogosort(nums)

b0b0b058d7843465de7c79558c8da350f1e291c8
ducks <mschaeffer53@gmail.com>
2017-10-25 12:26:38 -0700

    2nd revision

--- python/lab32_RainData.py
+++ python/lab32_RainData.py
@@ -67,5 +67,56 @@ std = math.sqrt(var)
 print(f'The standard deviation is {std}')
 
 #find the most rainy day
+max_rain = 0
 for line in data:
-    print(line[1])
+    if line[1] == '-' or line[1] == '':
+        continue
+    if int(line[1]) > max_rain:
+        max_rain = line[1]
+print(f'The rainiest day had {max_rain} hundredths of an inch of rain.')
+
+for line in data:
+    if line[1] == '-' or line[1] == '':
+        continue
+    if int(line[1]) == max_rain:
+        max_day = line[0]
+
+#print day, month, year of rainiest day
+print(f'The rainiest day was {max_day.month}/{max_day.day}/{max_day.year}.')
+
+#find unique years
+years = [] #list of unique years
+for line in data:
+    if line[0] == '-' or line[0] == '':
+        continue
+    if line[0].year in years:
+        continue
+    else: years.append(line[0].year)
+
+year_dict = {} #assign each year as a key in a dict
+for year in years:
+    year_dict[year] = 0
+
+
+#add total rain as values to year keys in dict
+for year in years:
+    total_rain = 0
+    for line in data:
+        if year == line[0].year:
+            if line[1] == '-' or line[1] == '':
+                continue
+            total_rain += int(line[1])
+    year_dict[year] = total_rain
+
+
+#find the rainiest year in a dictionary of years and rain values
+max_rain_val = 0
+for key in year_dict:
+    if year_dict[key] > max_rain_val:
+        max_rain_val = year_dict[key]
+
+for key, value in year_dict.items():
+    if value == max_rain_val:
+        max_rain_year = key
+
+print(f'The rainiest year was {max_rain_year}.')

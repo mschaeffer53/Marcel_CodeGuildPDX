21d8c0fbe3d699111ed5a93002305311b9e17060
ducks <mschaeffer53@gmail.com>
2017-12-01 16:22:45 -0800

    1st commit

--- django/mainsite/library/models.py
+++ django/mainsite/library/models.py
@@ -2,18 +2,21 @@ from django.db import models
 import datetime
 # Create your models here.
 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 class Book(models.Model):
     title = models.CharField(max_length=99)
     pub_date = models.DateField()
-    author = models.ManyToOneRel()
+    author = models.ForeignKey(Author)
 
     def __str__(self):
         return f'{self.title} {self.pub_date} {self.author}'
 
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f'{self.name}'
 

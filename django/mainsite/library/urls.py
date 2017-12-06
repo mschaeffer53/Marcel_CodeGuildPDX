3b9fcc4026e776e977c91f72d58764d859d20cf5
ducks <mschaeffer53@gmail.com>
2017-12-06 12:39:30 -0800

    3rd commit

--- django/mainsite/library/urls.py
+++ django/mainsite/library/urls.py
@@ -7,5 +7,6 @@ app_name = 'library'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^checkout/$', views.checkout, name='checkout'),
-    url(r'^checkin/$', views.checkin, name='checkin')
+    url(r'^checkin/$', views.checkin, name='checkin'),
+    url(r'^rentallog/$', views.rentallog, name='rentallog')
 ]
